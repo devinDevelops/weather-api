9d6064bf20b7f15967da4a4c126761f17192c9ca
Devin <devink12110@gmail.com>
2023-06-26 21:28:28 -0400

    added cors

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const conditionIMG = document.querySelector('.weather-condition');
 async function getWeather() {
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=5e03bd42c86845dc90d143729232406&q=${locationINPUT.value}&aqi=no`
+      `http://api.weatherapi.com/v1/current.json?key=5e03bd42c86845dc90d143729232406&q=${locationINPUT.value}&aqi=no`,
+      { mode: 'cors' }
     );
     const data = await response.json();
 
